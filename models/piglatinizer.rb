5c96964fde13f82e91e281a68615a28857536785
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-23 20:30:54 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,19 +2,14 @@ class PigLatinizer
 
   attr_reader :text
 
-  def pig_latinize(words)
-    #Take in string of words and make into an array
-    array = words.split(" ")
-    #Iterate over the array and return array with each word in Pig Latin
-    new_array = array.collect do |word|
-      if #word starts with a vowel, add "way"
-      else #word starts with consonant, move letters before vowel to end, then add "ay"
-
-
-      end
+  def piglatin(word)
+    if word =~ (/\A[aeiou]/i)
+      word = word + 'ay'
+    elsif word =~ (/\A[^aeiou]/i)
+      match = /\A[^aeiou]/i.match(word)
+      word = match.post_match + match.to_s + 'ay'
     end
-    #Return the array as a string
-    new_array.join(" ")
+    word
   end
 
 end
