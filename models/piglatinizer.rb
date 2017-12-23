0304427c11fc1926e620ae0340548d8618c111ee
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-23 20:40:31 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,7 +4,7 @@ class PigLatinizer
 
   def piglatin(word)
     if word =~ (/\A[aeiou]/i)
-      word = word + 'ay'
+      word = word + 'way'
     elsif word =~ (/\A[^aeiou]/i)
       match = /\A[^aeiou]/i.match(word)
       word = match.post_match + match.to_s + 'ay'
