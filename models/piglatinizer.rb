c21e01d04f8cb60dfe6f1cc98bd01b616584e033
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-23 20:25:03 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,6 +4,7 @@ class PigLatinizer
 
   def pig_latinize(words)
     #Take in string of words and make into an array
+    array = words.split(" ")
     #Iterate over the array and return array with each word in Pig Latin
     #Return the array as a string
   end
