dc0e4b292c1b7822f757f0d2f7414374d9e419c0
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-23 20:25:39 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -6,7 +6,10 @@ class PigLatinizer
     #Take in string of words and make into an array
     array = words.split(" ")
     #Iterate over the array and return array with each word in Pig Latin
+    new_array = array.collect do |word|
+    end
     #Return the array as a string
+    new_array.join(" ")
   end
 
 end
