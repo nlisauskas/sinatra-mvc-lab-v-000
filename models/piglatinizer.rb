1cd8e275a4e7039b919343e8b651322b223630d6
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-23 20:28:25 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,6 +7,11 @@ class PigLatinizer
     array = words.split(" ")
     #Iterate over the array and return array with each word in Pig Latin
     new_array = array.collect do |word|
+      if #word starts with a vowel, add "way"
+      else #word starts with consonant, move letters before vowel to end, then add "ay"
+
+
+      end
     end
     #Return the array as a string
     new_array.join(" ")
