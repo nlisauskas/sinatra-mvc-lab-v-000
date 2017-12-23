2f197979cd8dffba72ce8fbe6917ec59b18f965b
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-23 20:42:53 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,22 +2,25 @@ class PigLatinizer
 
   attr_reader :text
 
-  def piglatin(word)
-    if word =~ (/\A[aeiou]/i)
-      word = word + 'way'
-    elsif word =~ (/\A[^aeiou]/i)
-      match = /\A[^aeiou]/i.match(word)
-      word = match.post_match + match.to_s + 'ay'
-    end
-    word
-  end
+  def piglatinize(words)
+  if words[0].match(/a|e|i|o|u|A|E|I|O|U/)
+               words = words << "way"
+     elsif words[0].match(/[bcdfghjklmnpqrstvwxyz]/) && words[1].match(/[bcdfghjklmnpqrstvwxyz]/)&& words[2].match(/[bcdfghjklmnpqrstvwxyz]/)
+         words = words[3..-1]<<words[0..2]
+         words << "ay"
+     elsif words[0].match(/[bcdfghjklmnpqrstvwxyz]/) && words[1].match(/[bcdfghjklmnpqrstvwxyz]/)
+       words = words[2..-1]<<words[0..1]
+       words << "ay"
+     else
+       words = words[1..-1]<<words[0]
+       words << "ay"
+     end
+     words
+   end
 
-  def piglatinize(phrase)
-    new_array = phrase.split.collect do |word|
-    piglatin(word)
-  end
-  array = new_array.join(" ")
-  array
-  end
+   def to_pig_latin(words)
+     words.split.collect{|word| piglatinize(word)}.join(" ")
+
+   end
 
 end
