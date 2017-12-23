8fd959e3e769df0dddf0b9c6e678c410429b3446
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-23 20:51:43 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,25 +2,19 @@ class PigLatinizer
 
   attr_reader :text
 
-  def piglatinize(words)
-  if words[0].match(/a|e|i|o|u|A|E|I|O|U/)
-               words = words << "way"
-     elsif words[0].match(/[bcdfghjklmnpqrstvwxyz]/) && words[1].match(/[bcdfghjklmnpqrstvwxyz]/)&& words[2].match(/[bcdfghjklmnpqrstvwxyz]/)
-         words = words[3..-1]<<words[0..2]
-         words << "ay"
-     elsif words[0].match(/[bcdfghjklmnpqrstvwxyz]/) && words[1].match(/[bcdfghjklmnpqrstvwxyz]/)
-       words = words[2..-1]<<words[0..1]
-       words << "ay"
-     else
-       words = words[1..-1]<<words[0]
-       words << "ay"
-     end
-     words
-   end
-
-   def to_pig_latin(words)
-     words.split.collect{|word| piglatinize(word)}.join(" ")
-
-   end
+  +  def piglatinize(text)
+   +     text.split.map do |word|
+   +       if /\A[aeiou]/i.match(word)
+   +         "#{word}way"
+   +       else
+   +         cons = word.split(/[aeiou]/).first
+   +         "#{word.sub(cons,"")}#{cons}ay"
+   +       end
+   +       end.join(' ')
+   +     end
+   +
+   +   def to_pig_latin(text)
+   +     piglatinize(text)
+   +   end
 
 end
