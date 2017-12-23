29da523c47a62aa96e2aedd55d0692342008bbe9
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-23 20:14:59 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,16 +2,13 @@ class PigLatinizer
 
   attr_reader :text
 
-  def pig_latinize()
-    array = @string.split(' ')
-    new_array = array.collect do |word|
+  def pig_latinize(words)
+
       if word[0].match(/[aeiou]/)
         word << "way"
       elsif
         word[0].match(/[bcdfghjklmnpqrstvwxyz]/)
       end
     end
-    new_array.join(' ')
-  end
 
 end
